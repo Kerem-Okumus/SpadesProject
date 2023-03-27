9b6c4d83bec53c13a9460b162803f3d77884afef
Okumuss <okumuskerem15@gmail.com>
2023-03-27 21:56:56 +0300

    test

--- src/main/java/SpadesGame.java
+++ src/main/java/SpadesGame.java
@@ -10,7 +10,27 @@ deck.dealCards();
 
 
 
+/*   output template:
 
+(at the beginning)
+-deck has been created
+-deck has been shuffled
+-game has started
+-your hand = 1-(2ofDiamonds) ,2-(queenOfHearts) ... etc.
+-what is your bid?
+-bits have set : player(3) ,bot1(4) ,bot2(2) ,bot3(2)
+
+(each turn)
+-your current hand: 1-(2ofDiamonds) ,2-(queenOfHearts) ... etc.
+-please select a card to play (enter the index of it)
+-you played .... , bot1 played .... ,bot2 played ..... and bot3 played ....
+-..... won the round (n)
+-score table :player(1) ,bot1(0 ... etc.
+
+
+(at the end of the game)
+player won the game with bid: ... and score: ...
+ */
 
 
     }
